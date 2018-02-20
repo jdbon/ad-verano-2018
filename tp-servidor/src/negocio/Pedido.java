067cf7e60f749640d01c37e3234c2af306a93669
jdbon <jdbonelli@googlemail.com>
2018-02-20 18:27:33 -0300

    comenzando con pedido

--- tp-servidor/src/negocio/Pedido.java
+++ tp-servidor/src/negocio/Pedido.java
@@ -1,12 +1,12 @@
 package negocio;
 
 import java.sql.Date;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Pedido {
 
-	private static int numerador;
 	private Integer idPedido;
 	private List<ItemPedido> items;
 	private Cliente cliente;
@@ -17,9 +17,75 @@ public class Pedido {
 	private String motivoRechazo;
 	
 	public Pedido() {
-		numerador++;
-		this.idPedido = numerador;
+
 		this.items = new ArrayList<ItemPedido>();
+		this.estado = EstadoPedido.Pendiente;
+		this.fechaCreacion = Date.valueOf(LocalDate.now());
 		
+		
+	}
+
+	public Integer getIdPedido() {
+		return idPedido;
+	}
+
+	public void setIdPedido(Integer idPedido) {
+		this.idPedido = idPedido;
+	}
+
+	public List<ItemPedido> getItems() {
+		return items;
+	}
+
+	public void setItems(List<ItemPedido> items) {
+		this.items = items;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public EstadoPedido getEstado() {
+		return estado;
+	}
+
+	public void setEstado(EstadoPedido estado) {
+		this.estado = estado;
+	}
+
+	public Date getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(Date fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	public Date getFechaEntregaEstimada() {
+		return fechaEntregaEstimada;
+	}
+
+	public void setFechaEntregaEstimada(Date fechaEntregaEstimada) {
+		this.fechaEntregaEstimada = fechaEntregaEstimada;
+	}
+
+	public String getMotivoRechazo() {
+		return motivoRechazo;
+	}
+
+	public void setMotivoRechazo(String motivoRechazo) {
+		this.motivoRechazo = motivoRechazo;
 	}
 }
