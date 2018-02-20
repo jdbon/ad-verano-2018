a3f4415ad33f4d599a5ca23a9278e850dd74502a
yo <yo@192.168.210.162>
2018-02-20 20:46:23 -0300

    estado + consturtor + gett&sett

--- tp-servidor/src/negocio/Articulo.java
+++ tp-servidor/src/negocio/Articulo.java
@@ -1,5 +1,116 @@
 package negocio;
 
+import java.util.*;
+
 public class Articulo {
+	
+	private Integer codigoBarra;
+	private String descripcion;
+	private Presentacion presentacion;
+	private Integer tamaño;
+	private Integer unidad;
+	private Float precioVenta;
+	private Integer cantidadOrdenDeCompra;
+	private List<Lote> lotes;
+	private Integer cantidadReservada;
+	private List<Movimiento> movimientos;
+	
+	public Articulo() {
+		super();
+		lotes = new ArrayList<Lote>();
+		movimientos = new ArrayList<Movimiento>();
+	}
+
+	public Integer getCodigoBarra() {
+		return codigoBarra;
+	}
+
+	public void setCodigoBarra(Integer codigoBarra) {
+		this.codigoBarra = codigoBarra;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Presentacion getPresentacion() {
+		return presentacion;
+	}
+
+	public void setPresentacion(Presentacion presentacion) {
+		this.presentacion = presentacion;
+	}
+
+	public Integer getTamaño() {
+		return tamaño;
+	}
+
+	public void setTamaño(Integer tamaño) {
+		this.tamaño = tamaño;
+	}
+
+	public Integer getUnidad() {
+		return unidad;
+	}
+
+	public void setUnidad(Integer unidad) {
+		this.unidad = unidad;
+	}
+
+	public Float getPrecioVenta() {
+		return precioVenta;
+	}
+
+	public void setPrecioVenta(Float precioVenta) {
+		this.precioVenta = precioVenta;
+	}
+
+	public Integer getCantidadOrdenDeCompra() {
+		return cantidadOrdenDeCompra;
+	}
+
+	public void setCantidadOrdenDeCompra(Integer cantidadOrdenDeCompra) {
+		this.cantidadOrdenDeCompra = cantidadOrdenDeCompra;
+	}
+
+	public Integer getCantidadReservada() {
+		return cantidadReservada;
+	}
+
+	public void setCantidadReservada(Integer cantidadReservada) {
+		this.cantidadReservada = cantidadReservada;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigoBarra == null) ? 0 : codigoBarra.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Articulo other = (Articulo) obj;
+		if (codigoBarra == null) {
+			if (other.codigoBarra != null)
+				return false;
+		} else if (!codigoBarra.equals(other.codigoBarra))
+			return false;
+		return true;
+	}
+	
+	
+	
 
 }
