179db62a1800066de861fcd00a56c7946553c96d
alu602 <alu602@602PC20.labs>
2018-02-20 22:03:14 -0300

    negocio itemFactura

--- tp-servidor/src/negocio/ItemFactura.java
+++ tp-servidor/src/negocio/ItemFactura.java
@@ -1,5 +1,34 @@
 package negocio;
 
 public class ItemFactura {
+	
+	private Articulo articulo;
+	private Integer cantidad;
+	private float subTotal;
+	
+	public ItemFactura(){
+		super();
+	}
+	
+	public Articulo getArticulo() {
+		return articulo;
+	}
+	public void setArticulo(Articulo articulo) {
+		this.articulo = articulo;
+	}
+	public Integer getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+	public float getSubTotal() {
+		return subTotal;
+	}
+	public void setSubTotal(float subTotal) {
+		this.subTotal = subTotal;
+	}
+	
+	
 
 }
