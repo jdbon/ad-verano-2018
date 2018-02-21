b498b5ab6aac6515c34942f376855d768b8aa842
alu602 <alu602@602PC20.labs>
2018-02-21 19:00:07 -0300

    entity cliente

--- tp-servidor/src/entity/ClienteEntity.java
+++ tp-servidor/src/entity/ClienteEntity.java
@@ -1,5 +1,57 @@
 package entity;
 
 public class ClienteEntity {
+	
+	private Integer dni;
+	private String apellido;
+	private String nombre;
+	private float limiteCredito;
+	private float saldoActual;
+	
+	public ClienteEntity() {
+		super();
+		
+	}
+
+	public Integer getDni() {
+		return dni;
+	}
+
+	public void setDni(Integer dni) {
+		this.dni = dni;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public float getLimiteCredito() {
+		return limiteCredito;
+	}
+
+	public void setLimiteCredito(float limiteCredito) {
+		this.limiteCredito = limiteCredito;
+	}
+
+	public float getSaldoActual() {
+		return saldoActual;
+	}
+
+	public void setSaldoActual(float saldoActual) {
+		this.saldoActual = saldoActual;
+	}
+
 
 }
