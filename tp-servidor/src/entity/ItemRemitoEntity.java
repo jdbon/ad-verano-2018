041983a156dfb5e4a286553b0a15e44c3703ca6d
tetip <pablo.teti@gmail.com>
2018-02-20 21:21:21 -0300

    agrego ArticuloEntity

--- tp-servidor/src/entity/ItemRemitoEntity.java
+++ tp-servidor/src/entity/ItemRemitoEntity.java
@@ -1,5 +1,14 @@
 package entity;
 
+import negocio.Articulo;
+
 public class ItemRemitoEntity {
+	
+	private Articulo articulo;
+	private Integer cantidad;
+	
+	public ItemRemitoEntity() {
+		super();
+	}
 
 }
