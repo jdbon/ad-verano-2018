d5633f302f0f8b7aa059044f8aee4586af7812a2
bzlm99 <29681277+bzlm99@users.noreply.github.com>
2018-02-21 16:13:46 -0300

    negocio cliente

--- tp-servidor/src/negocio/Cliente.java
+++ tp-servidor/src/negocio/Cliente.java
@@ -1,5 +1,60 @@
 package negocio;
 
 public class Cliente {
+	
+	private Integer dni;
+	private String apellido;
+	private String nombre;
+	private float limiteCredito;
+	private float saldoActual;
+	
+	public Cliente() {
+		super();
+		
+	}
+
+	public Integer getDni() {
+		return dni;
+	}
+
+	public void setDni(Integer dni) {
+		this.dni = dni;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public float getLimiteCredito() {
+		return limiteCredito;
+	}
+
+	public void setLimiteCredito(float limiteCredito) {
+		this.limiteCredito = limiteCredito;
+	}
+
+	public float getSaldoActual() {
+		return saldoActual;
+	}
+
+	public void setSaldoActual(float saldoActual) {
+		this.saldoActual = saldoActual;
+	}
+	
+	
+	
+	
 
 }
