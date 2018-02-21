b5d16ae811ee42d0c6414a4e262c0099fbb6320b
alu602 <alu602@602PC20.labs>
2018-02-20 22:09:06 -0300

    negocio Movimiento

--- tp-servidor/src/negocio/Movimiento.java
+++ tp-servidor/src/negocio/Movimiento.java
@@ -1,5 +1,41 @@
 package negocio;
 
 public abstract class Movimiento {
+	
+	private Integer nroMovimiento;
+	private Integer cantidad;
+	private Articulo articulo;
+	private TipoMovimiento tipo;
+	
+	public Movimiento() {
+		super();
+	}
+	
+	public Integer getNroMovimiento() {
+		return nroMovimiento;
+	}
+	public void setNroMovimiento(Integer nroMovimiento) {
+		this.nroMovimiento = nroMovimiento;
+	}
+	public Integer getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+	public Articulo getArticulo() {
+		return articulo;
+	}
+	public void setArticulo(Articulo articulo) {
+		this.articulo = articulo;
+	}
+	public TipoMovimiento getTipo() {
+		return tipo;
+	}
+	public void setTipo(TipoMovimiento tipo) {
+		this.tipo = tipo;
+	}
+	
+	
 
 }
