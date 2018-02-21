c2868384623b309b2572eccc0cd807c09189df0d
alu602 <alu602@602PC20.labs>
2018-02-20 22:05:54 -0300

    negocio ItemPedido

--- tp-servidor/src/negocio/ItemPedido.java
+++ tp-servidor/src/negocio/ItemPedido.java
@@ -1,5 +1,42 @@
 package negocio;
 
 public class ItemPedido {
+	
+	private Articulo articulo;
+	private Integer cantidadSolicitada;
+	private Integer cantidadReservada;
+	private float subTotal;
+	
+	public ItemPedido() {
+		super();
+		
+	}
+	
+	public Articulo getArticulo() {
+		return articulo;
+	}
+	public void setArticulo(Articulo articulo) {
+		this.articulo = articulo;
+	}
+	public Integer getCantidadSolicitada() {
+		return cantidadSolicitada;
+	}
+	public void setCantidadSolicitada(Integer cantidadSolicitada) {
+		this.cantidadSolicitada = cantidadSolicitada;
+	}
+	public Integer getCantidadReservada() {
+		return cantidadReservada;
+	}
+	public void setCantidadReservada(Integer cantidadReservada) {
+		this.cantidadReservada = cantidadReservada;
+	}
+	public float getSubTotal() {
+		return subTotal;
+	}
+	public void setSubTotal(float subTotal) {
+		this.subTotal = subTotal;
+	}
+	
+	
 
 }
