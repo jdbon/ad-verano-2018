8edfdb3bfbdda9ec3124fa99a89d2e61703e879f
tetip <pablo.teti@gmail.com>
2018-02-20 21:19:28 -0300

    agrego estado a ItemRemito

--- tp-servidor/src/negocio/ItemRemito.java
+++ tp-servidor/src/negocio/ItemRemito.java
@@ -1,5 +1,33 @@
 package negocio;
 
 public class ItemRemito {
+	
+	private Articulo articulo;
+	private Integer cantidad;
+	
+	public ItemRemito(Articulo articulo, Integer cantidad) {
+		super();
+		this.articulo = articulo;
+		this.cantidad = cantidad;
+	}
+
+	public Articulo getArticulo() {
+		return articulo;
+	}
+
+	public void setArticulo(Articulo articulo) {
+		this.articulo = articulo;
+	}
+
+	public Integer getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+	
+	
 
 }
