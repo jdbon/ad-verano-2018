85f996bd258e5db52b464108850a2f1b6b72b6a1
alu602 <alu602@602PC20.labs>
2018-02-21 21:58:56 -0300

    clienteDAO clienteEntity Ubicacion

--- tp-servidor/src/entity/ClienteEntity.java
+++ tp-servidor/src/entity/ClienteEntity.java
@@ -2,7 +2,7 @@ package entity;
 
 public class ClienteEntity {
 	
-	private Integer dni;
+	private Integer idCliente;
 	private String apellido;
 	private String nombre;
 	private float limiteCredito;
@@ -13,12 +13,12 @@ public class ClienteEntity {
 		
 	}
 
-	public Integer getDni() {
-		return dni;
+	public Integer getidCliente() {
+		return idCliente;
 	}
 
-	public void setDni(Integer dni) {
-		this.dni = dni;
+	public void setidCliente(Integer idCliente) {
+		this.idCliente = idCliente;
 	}
 
 	public String getApellido() {
