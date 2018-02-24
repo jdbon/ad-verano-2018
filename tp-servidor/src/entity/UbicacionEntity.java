93f9ca5b9229b91298ef1d98be4efff0c18562ab
bzlm99 <29681277+bzlm99@users.noreply.github.com>
2018-02-24 19:11:12 -0300

    UbicacionEntity

--- tp-servidor/src/entity/UbicacionEntity.java
+++ tp-servidor/src/entity/UbicacionEntity.java
@@ -1,15 +1,31 @@
 package entity;
 
+import javax.persistence.*;
+
 import enumerator.Bloque;
 import enumerator.Calle;
 import enumerator.Estanteria;
 import enumerator.Posicion;
 
+@Entity
+@Table(name="ubicaciones")
 public class UbicacionEntity {
 	
-	private static int cantidadActual;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="ubi_id")
+	private Integer idUbicacion;
+	
+	@Column(name="ubi_tca")
+	private int cantidadActual;
+	
+	@OneToOne
+	@JoinColumn(name="ubi_art")
 	private ArticuloEntity articulo;
+	
+	@Column(name="ubi_cod")
 	private String codigo;
+	
 	private Calle calle;
 	private Bloque bloque;
 	private Estanteria estanteria;
@@ -19,12 +35,20 @@ public class UbicacionEntity {
 		
 	}
 
-	public static int getCantidadActual() {
+	public Integer getIdUbicacion() {
+		return idUbicacion;
+	}
+
+	public void setIdUbicacion(Integer idUbicacion) {
+		this.idUbicacion = idUbicacion;
+	}
+
+	public int getCantidadActual() {
 		return cantidadActual;
 	}
 
-	public static void setCantidadActual(int cantidadActual) {
-		UbicacionEntity.cantidadActual = cantidadActual;
+	public void setCantidadActual(int cantidadActual) {
+		this.cantidadActual = cantidadActual;
 	}
 
 	public ArticuloEntity getArticulo() {
