e0b7c6fdb721d91f667de865bd75dab469b9873e
bzlm99 <29681277+bzlm99@users.noreply.github.com>
2018-02-24 20:12:52 -0300

    ItemRemitoEntity

--- tp-servidor/src/entity/ItemRemitoEntity.java
+++ tp-servidor/src/entity/ItemRemitoEntity.java
@@ -1,10 +1,30 @@
 package entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
 import negocio.Articulo;
 
+@Entity
+@Table(name="item_remitos")
 public class ItemRemitoEntity {
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="itr_id")
+	private Integer idItemRemito;
+	
+	@OneToOne
+	@JoinColumn(name="itr_art")
 	private Articulo articulo;
+	
+	@Column(name="itr_cant")
 	private Integer cantidad;
 	
 	public ItemRemitoEntity() {}
