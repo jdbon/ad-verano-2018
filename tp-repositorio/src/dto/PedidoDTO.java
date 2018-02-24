070c1d4fc13090eddca14a3fa2cc29f222c9a4ae
Enzo <Enzo@10.131.146.77>
2018-02-24 17:18:11 -0300

    metodo buscarpedidos pendientes

--- tp-repositorio/src/dto/PedidoDTO.java
+++ tp-repositorio/src/dto/PedidoDTO.java
@@ -4,9 +4,6 @@ import java.io.Serializable;
 import java.sql.Date;
 import java.util.List;
 
-import negocio.Cliente;
-import negocio.EstadoPedido;
-import negocio.ItemPedido;
 
 public class PedidoDTO implements Serializable {
 
@@ -19,7 +16,7 @@ public class PedidoDTO implements Serializable {
 	private List<ItemPedidoDTO> items;
 	private ClienteDTO cliente;
 	private String direccion;
-	private EstadoPedido estado;
+	//private EstadoPedido estado;
 	private Date fechaCreacion;
 	private Date fechaEntregaEstimada;
 	private String motivoRechazo;
