4aa35b11beb6e18deb46cc3863c12ce11a7f72f4
Pablo <Pablo@10.0.2.15>
2018-02-25 20:46:04 -0300

    Save de Pedido

--- tp-servidor/src/controlador/ControladorDeClientes.java
+++ tp-servidor/src/controlador/ControladorDeClientes.java
@@ -2,9 +2,11 @@ package controlador;
 
 import java.util.List;
 
+import dao.PedidoDAO;
 import dto.ItemPedidoDTO;
 import excepcion.ArticuloException;
 import excepcion.ClienteException;
+import excepcion.PedidoException;
 import negocio.Pedido;
 
 public class ControladorDeClientes {
@@ -19,12 +21,12 @@ public class ControladorDeClientes {
 		return instancia;
 	}
 	
-	public Integer generarNuevoPedido(int idCliente, String direccion, List<ItemPedidoDTO> items ) throws ClienteException, ArticuloException {
+	public Integer generarNuevoPedido(int idCliente, String direccion, List<ItemPedidoDTO> items ) throws ClienteException, ArticuloException, PedidoException {
 		Pedido pedido = new Pedido(idCliente, direccion);
 		for(ItemPedidoDTO item: items) {
 			pedido.agregarItemPedido(item.getArticulo().getCodigoBarra(), item.getCantidadReservada());
 		}		
-		
+		PedidoDAO.getInstancia().save(pedido);
 		return pedido.getIdPedido();
 	}
 }
