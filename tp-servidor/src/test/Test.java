b908006a0f6c192512ae7ef8c30b515d2523a583
Pablo <Pablo@10.0.2.15>
2018-02-25 22:05:13 -0300

    Pruebas Lista de Articulos

--- tp-servidor/src/test/Test.java
+++ tp-servidor/src/test/Test.java
@@ -6,25 +6,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 import controlador.ControladorDeClientes;
-import controlador.ControladorDeDespacho;
-import dao.ClienteDAO;
-import dao.PedidoDAO;
 import dto.ArticuloDTO;
-import dto.ItemPedidoDTO;
-import dto.PedidoDTO;
-import enumerator.TipoFactura;
 import excepcion.ArticuloException;
 import excepcion.ClienteException;
-import excepcion.FacturaException;
 import excepcion.PedidoException;
-import negocio.*;
 
 public class Test {
 
 	public static void main(String[] args) throws ClienteException, ArticuloException, PedidoException {
 		
 		CargaDeDatos.cargarClientes();
-		ControladorDeClientes.getInstancia().getAllArticulos();
+		@SuppressWarnings("unused")
+		List<ArticuloDTO> articulos = ControladorDeClientes.getInstancia().getAllArticulos();
+		System.out.println("TENGO EL LISTADO");
 		
 		//ControladorDeClientes.getInstancia().generarNuevoPedido(1, "La recalcada concha de tu hermana", items);
 		
